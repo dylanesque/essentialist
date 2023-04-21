d8c517a1e44cd784c5f4d0d1354e3ca340144cf6
michaelc <dylanesque27@gmail.com>
2023-04-21 12:39:08 -0500

    Add basic test of function argument

--- 1_The_Physical/Guess_Point_Level_1_Stateless/1_Red_Green_Refactor/1_1_Fizzbuzz/src/index.spec.ts
+++ 1_The_Physical/Guess_Point_Level_1_Stateless/1_Red_Green_Refactor/1_1_Fizzbuzz/src/index.spec.ts
@@ -1,5 +1,12 @@
+import { fizzbuzz } from './fizzbuzz';
 
 describe("fizzbuzz", () => {
-
+    it('takes a positive number', () => {
+        const num = 3;
+    
+        expect(fizzbuzz(num)).toEqual(num);
+    })
 
 });
+
+
