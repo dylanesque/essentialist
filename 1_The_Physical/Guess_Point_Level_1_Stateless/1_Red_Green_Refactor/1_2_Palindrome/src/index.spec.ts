cbd2f8178e12ace4c910b9cf0d79ea75cfe106c7
michaelc <dylanesque27@gmail.com>
2023-05-05 15:11:37 -0400

    Add test cases and functionality for multi-word strings and case normalization

--- 1_The_Physical/Guess_Point_Level_1_Stateless/1_Red_Green_Refactor/1_2_Palindrome/src/index.spec.ts
+++ 1_The_Physical/Guess_Point_Level_1_Stateless/1_Red_Green_Refactor/1_2_Palindrome/src/index.spec.ts
@@ -1,7 +1,30 @@
-import { reverseAString } from './index';
+import { checkPalindrome, reverseAString } from './index';
 
 describe('palindrome checker', () => {
     it('reverses a string', () => {
         expect(reverseAString('bill')).toBe('llib');
     });
+
+    it('returns false if it is not a palindrome', () => {
+        expect(checkPalindrome('bill')).toBe(false);
+    });
+
+    it('returns true if it is a palindrome', () => {
+        expect(checkPalindrome('mom')).toBe(true);
+    });
+
+    it('reverses a multi-word string', () => {
+        expect(reverseAString('ball hitter')).toBe('rettih llab');
+    });
+
+    it('returns false for multi-word non-palindrome', () => {
+        expect(checkPalindrome('ball hitter')).toBe(false);
+    });
+
+    it('returns true for multi-word palindrome', () => {
+        expect(checkPalindrome('never odd or even')).toBe(true);
+    });
+
+
 })
+
--- 1_The_Physical/Guess_Point_Level_1_Stateless/1_Red_Green_Refactor/1_2_Palindrome/src/index.ts
+++ 1_The_Physical/Guess_Point_Level_1_Stateless/1_Red_Green_Refactor/1_2_Palindrome/src/index.ts
@@ -1,3 +1,11 @@
 export function reverseAString(str: string) {
-    return str.split("").reverse().join("");
+    return str.toLowerCase().split("").reverse().join("");
+}
+
+export function checkPalindrome(str: string) {
+    let reversed = reverseAString(str);
+
+    str = str.replace(/\s+/g, '');
+    reversed = reversed.replace(/\s+/g, '');
+    return (str == reversed) ? true : false;
 }
