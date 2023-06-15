c8390e86322308480633dd26de822a5bbed24644
michaelc <dylanesque27@gmail.com>
2023-06-15 14:03:37 -0400

    Fix comparison algorithm in checkPalindrome function to fully handle casing differences.

--- 1_The_Physical/Guess_Point_Level_1_Stateless/1_Red_Green_Refactor/1_2_Palindrome/src/index.spec.ts
+++ 1_The_Physical/Guess_Point_Level_1_Stateless/1_Red_Green_Refactor/1_2_Palindrome/src/index.spec.ts
@@ -1,30 +1,31 @@
-import { checkPalindrome, reverseAString } from './index';
+import { checkPalindrome, reverseAString } from "./index";
 
-describe('palindrome checker', () => {
-    it('reverses a string', () => {
-        expect(reverseAString('bill')).toBe('llib');
-    });
+describe("palindrome checker", () => {
+  it("reverses a string", () => {
+    expect(reverseAString("bill")).toBe("llib");
+  });
 
-    it('returns false if it is not a palindrome', () => {
-        expect(checkPalindrome('bill')).toBe(false);
-    });
+  it('knows that "bill" is not a palindrome', () => {
+    expect(checkPalindrome("bill")).toBe(false);
+  });
 
-    it('returns true if it is a palindrome', () => {
-        expect(checkPalindrome('mom')).toBe(true);
-    });
+  it.each([
+    ["mom", true],
+    ["Mom", true],
+    ["MoM", true],
+  ])("knows that %p is a palindrome", (word: string, status: boolean) => {
+    expect(checkPalindrome(word)).toBe(status);
+  });
 
-    it('reverses a multi-word string', () => {
-        expect(reverseAString('ball hitter')).toBe('rettih llab');
-    });
+  it("reverses a multi-word string", () => {
+    expect(reverseAString("ball hitter")).toBe("rettih llab");
+  });
 
-    it('returns false for multi-word non-palindrome', () => {
-        expect(checkPalindrome('ball hitter')).toBe(false);
-    });
+  it("returns false for multi-word non-palindrome", () => {
+    expect(checkPalindrome("ball hitter")).toBe(false);
+  });
 
-    it('returns true for multi-word palindrome', () => {
-        expect(checkPalindrome('never odd or even')).toBe(true);
-    });
-
-
-})
-
+  it("returns true for multi-word palindrome", () => {
+    expect(checkPalindrome("never odd or even")).toBe(true);
+  });
+});
--- 1_The_Physical/Guess_Point_Level_1_Stateless/1_Red_Green_Refactor/1_2_Palindrome/src/index.ts
+++ 1_The_Physical/Guess_Point_Level_1_Stateless/1_Red_Green_Refactor/1_2_Palindrome/src/index.ts
@@ -1,11 +1,11 @@
 export function reverseAString(str: string) {
-    return str.toLowerCase().split("").reverse().join("");
+    return str.split("").reverse().join("");
 }
 
 export function checkPalindrome(str: string) {
-    let reversed = reverseAString(str);
-
-    str = str.replace(/\s+/g, '');
+    let normalizedStr = str.toLowerCase();
+    let reversed = reverseAString(normalizedStr);
+    str = normalizedStr.replace(/\s+/g, '');
     reversed = reversed.replace(/\s+/g, '');
-    return (str == reversed) ? true : false;
+    return (str == reversed);
 }
