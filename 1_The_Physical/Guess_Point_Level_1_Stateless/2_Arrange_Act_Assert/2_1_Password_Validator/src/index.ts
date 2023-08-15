9b55e050d233ff3ce075260ab680b35159ea0c69
michaelc <dylanesque27@gmail.com>
2023-08-15 14:34:50 -0400

    first passing test, assert "no uppercase chars" failure

--- 1_The_Physical/Guess_Point_Level_1_Stateless/2_Arrange_Act_Assert/2_1_Password_Validator/src/index.ts
+++ 1_The_Physical/Guess_Point_Level_1_Stateless/2_Arrange_Act_Assert/2_1_Password_Validator/src/index.ts
@@ -1,11 +1,22 @@
 export function passwordValidator(password: string): boolean {
+    let hasUpper = false;
+    password.split('').forEach((char) => {
+        console.log(char);
+        if (char === char.toUpperCase() && char !== char.toLowerCase()) {
+            hasUpper = true;
+        }
+    })
+
+    if (hasUpper == false) { return false }
+
     if (password.length < 5 || password.length > 15) {
         return false;
     }
 
+    // checks string for numerical characters
     if (/\d/.test(password) == false) {
         return false;
     }
 
     return true
-} 
+}
