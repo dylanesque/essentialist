a8e567420ec3d6d06ab894945550639744b963b7
michaelc <dylanesque27@gmail.com>
2023-08-15 13:33:11 -0400

    first failing test, assert "too short" failure

--- 1_The_Physical/Guess_Point_Level_1_Stateless/2_Arrange_Act_Assert/2_1_Password_Validator/src/index.spec.ts
+++ 1_The_Physical/Guess_Point_Level_1_Stateless/2_Arrange_Act_Assert/2_1_Password_Validator/src/index.spec.ts
@@ -1,5 +1,25 @@
+/* 
+
+The system under test here should meet the following requirements:
+
+- Between 5 and 15 characters long
+
+- Contains at least one digit
+
+- Contains at least one upper case letter
+
+- Return an object containing a boolean result and an errors key that — when 
+provided with an invalid password — contains an error message or type for 
+all errors in occurrence. There can be multiple errors at a single time.
+
+*/
+
 
 describe('password validator', () => {
-
+    describe('length', () => {
+        it('does not validate passwords fewer than 5 characters', () => {
+            expect(passwordValidator('1huM')).toBe('false');
+        })
+    })
 })
 
